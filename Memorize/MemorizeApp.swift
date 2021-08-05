1a2590741716ed9371f8163b713a409e5578d16f
anderson <anderson@camerite.com>
2021-08-05 18:27:40 -0300

    create new project

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Daniel Silveira on 05/08/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
